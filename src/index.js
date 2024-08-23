24491b94a7fed71510ad85407c5356a6556fac6c
Sergey Scotcelias <sergey.sckotcelias@yandex.ru>
2024-08-23 12:09:09 +0800

    feat: add tab-switching logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,44 @@
 import './style.css';
-import renderHomePage from './renderHomePage';
 
-renderHomePage();
+import renderHomePage from './renderHomePage';
+import renderMenuPage from './renderMenuPage';
+import renderContactsPage from './renderContactsPage';
+
+const rendererFunctions = {
+  renderHomePage,
+  renderMenuPage,
+  renderContactsPage,
+};
+
+const content = document.querySelector('.content');
+const navList = document.querySelector('.nav-list');
+
+let currentPageName = 'Home';
+
+function clearContent(content) {
+  while (content.firstChild) {
+    content.lastChild.remove();
+  }
+}
+
+function handleTabSwitch(e) {
+  const target = e.target;
+  if (!target.classList.contains('nav-list__button')) return;
+
+  const pageName = target.id;
+  if (pageName === currentPageName) return;
+
+  currentPageName = pageName;
+
+  clearContent(content);
+
+  const rendererName = `render${pageName}Page`;
+
+  const { [rendererName]: renderer } = rendererFunctions;
+
+  renderer(content);
+}
+
+navList.addEventListener('click', handleTabSwitch);
+
+renderHomePage(content);
